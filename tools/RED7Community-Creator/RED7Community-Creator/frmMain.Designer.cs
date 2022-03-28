01318762dc0da4d52cae40b0c6308be97acee5de
Repo Admin <68230846+Creaous@users.noreply.github.com>
2022-03-28 23:33:47 +1030

    Updates for the Creator Tool
    - Removed the unused Utils form.
    - Fixed a typo in the config.php generation.
    - Removed all owners on items to prevent errors.
    - Hopefully fixed the auto increment on users.

--- tools/RED7Community-Creator/RED7Community-Creator/frmMain.Designer.cs
+++ tools/RED7Community-Creator/RED7Community-Creator/frmMain.Designer.cs
@@ -49,8 +49,6 @@
             this.ds_server = new System.Windows.Forms.TextBox();
             this.label8 = new System.Windows.Forms.Label();
             this.gbSQLSettings = new System.Windows.Forms.GroupBox();
-            this.ss_adminSiteName = new System.Windows.Forms.TextBox();
-            this.label14 = new System.Windows.Forms.Label();
             this.ss_maintenanceMode = new System.Windows.Forms.TextBox();
             this.label15 = new System.Windows.Forms.Label();
             this.ss_appealEmail = new System.Windows.Forms.TextBox();
@@ -92,7 +90,7 @@
             this.gbGeneralSettings.Font = new System.Drawing.Font("Segoe UI", 13F);
             this.gbGeneralSettings.Location = new System.Drawing.Point(697, 13);
             this.gbGeneralSettings.Name = "gbGeneralSettings";
-            this.gbGeneralSettings.Size = new System.Drawing.Size(336, 414);
+            this.gbGeneralSettings.Size = new System.Drawing.Size(336, 313);
             this.gbGeneralSettings.TabIndex = 0;
             this.gbGeneralSettings.TabStop = false;
             this.gbGeneralSettings.Text = "General Settings (config.php)";
@@ -110,7 +108,7 @@
             // 
             this.label17.AutoSize = true;
             this.label17.Font = new System.Drawing.Font("Segoe UI", 12F);
-            this.label17.Location = new System.Drawing.Point(6, 249);
+            this.label17.Location = new System.Drawing.Point(3, 249);
             this.label17.Name = "label17";
             this.label17.Size = new System.Drawing.Size(141, 21);
             this.label17.TabIndex = 11;
@@ -129,7 +127,7 @@
             // 
             this.label18.AutoSize = true;
             this.label18.Font = new System.Drawing.Font("Segoe UI", 12F);
-            this.label18.Location = new System.Drawing.Point(6, 193);
+            this.label18.Location = new System.Drawing.Point(3, 193);
             this.label18.Name = "label18";
             this.label18.Size = new System.Drawing.Size(88, 21);
             this.label18.TabIndex = 9;
@@ -148,7 +146,7 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Segoe UI", 12F);
-            this.label4.Location = new System.Drawing.Point(6, 137);
+            this.label4.Location = new System.Drawing.Point(3, 137);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(99, 21);
             this.label4.TabIndex = 7;
@@ -167,7 +165,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Segoe UI", 12F);
-            this.label2.Location = new System.Drawing.Point(6, 81);
+            this.label2.Location = new System.Drawing.Point(3, 81);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(69, 21);
             this.label2.TabIndex = 3;
@@ -186,7 +184,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI", 12F);
-            this.label1.Location = new System.Drawing.Point(6, 25);
+            this.label1.Location = new System.Drawing.Point(3, 25);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(81, 21);
             this.label1.TabIndex = 1;
@@ -223,7 +221,7 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Segoe UI", 12F);
-            this.label5.Location = new System.Drawing.Point(6, 193);
+            this.label5.Location = new System.Drawing.Point(3, 193);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(55, 21);
             this.label5.TabIndex = 7;
@@ -242,7 +240,7 @@
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Segoe UI", 12F);
-            this.label6.Location = new System.Drawing.Point(6, 137);
+            this.label6.Location = new System.Drawing.Point(3, 137);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(79, 21);
             this.label6.TabIndex = 5;
@@ -261,7 +259,7 @@
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Segoe UI", 12F);
-            this.label7.Location = new System.Drawing.Point(6, 81);
+            this.label7.Location = new System.Drawing.Point(3, 81);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(84, 21);
             this.label7.TabIndex = 3;
@@ -280,7 +278,7 @@
             // 
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Segoe UI", 12F);
-            this.label8.Location = new System.Drawing.Point(6, 25);
+            this.label8.Location = new System.Drawing.Point(3, 25);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(58, 21);
             this.label8.TabIndex = 1;
@@ -288,8 +286,6 @@
             // 
             // gbSQLSettings
             // 
-            this.gbSQLSettings.Controls.Add(this.ss_adminSiteName);
-            this.gbSQLSettings.Controls.Add(this.label14);
             this.gbSQLSettings.Controls.Add(this.ss_maintenanceMode);
             this.gbSQLSettings.Controls.Add(this.label15);
             this.gbSQLSettings.Controls.Add(this.ss_appealEmail);
@@ -307,29 +303,10 @@
             this.gbSQLSettings.Font = new System.Drawing.Font("Segoe UI", 13F);
             this.gbSQLSettings.Location = new System.Drawing.Point(12, 13);
             this.gbSQLSettings.Name = "gbSQLSettings";
-            this.gbSQLSettings.Size = new System.Drawing.Size(336, 485);
+            this.gbSQLSettings.Size = new System.Drawing.Size(336, 425);
             this.gbSQLSettings.TabIndex = 10;
             this.gbSQLSettings.TabStop = false;
             this.gbSQLSettings.Text = "SQL Settings:";
-            // 
-            // ss_adminSiteName
-            // 
-            this.ss_adminSiteName.Font = new System.Drawing.Font("Segoe UI", 12F);
-            this.ss_adminSiteName.Location = new System.Drawing.Point(7, 441);
-            this.ss_adminSiteName.Name = "ss_adminSiteName";
-            this.ss_adminSiteName.Size = new System.Drawing.Size(320, 29);
-            this.ss_adminSiteName.TabIndex = 16;
-            this.ss_adminSiteName.Text = "RED7Community Admin Panel";
-            // 
-            // label14
-            // 
-            this.label14.AutoSize = true;
-            this.label14.Font = new System.Drawing.Font("Segoe UI", 12F);
-            this.label14.Location = new System.Drawing.Point(6, 417);
-            this.label14.Name = "label14";
-            this.label14.Size = new System.Drawing.Size(135, 21);
-            this.label14.TabIndex = 15;
-            this.label14.Text = "Admin Site Name:";
             // 
             // ss_maintenanceMode
             // 
@@ -344,7 +321,7 @@
             // 
             this.label15.AutoSize = true;
             this.label15.Font = new System.Drawing.Font("Segoe UI", 12F);
-            this.label15.Location = new System.Drawing.Point(6, 361);
+            this.label15.Location = new System.Drawing.Point(3, 361);
             this.label15.Name = "label15";
             this.label15.Size = new System.Drawing.Size(146, 21);
             this.label15.TabIndex = 13;
@@ -363,7 +340,7 @@
             // 
             this.label16.AutoSize = true;
             this.label16.Font = new System.Drawing.Font("Segoe UI", 12F);
-            this.label16.Location = new System.Drawing.Point(6, 305);
+            this.label16.Location = new System.Drawing.Point(3, 305);
             this.label16.Name = "label16";
             this.label16.Size = new System.Drawing.Size(103, 21);
             this.label16.TabIndex = 11;
@@ -376,14 +353,13 @@
             this.ss_verifiedIcon.Name = "ss_verifiedIcon";
             this.ss_verifiedIcon.Size = new System.Drawing.Size(320, 29);
             this.ss_verifiedIcon.TabIndex = 10;
-            this.ss_verifiedIcon.Text = "https://cdn.jsdelivr.net/gh/RED7Studios/RED7Community-CDN@main/assets/images/veri" +
-    "fied.png";
+            this.ss_verifiedIcon.Text = "/assets/images/verified.png";
             // 
             // label13
             // 
             this.label13.AutoSize = true;
             this.label13.Font = new System.Drawing.Font("Segoe UI", 12F);
-            this.label13.Location = new System.Drawing.Point(6, 249);
+            this.label13.Location = new System.Drawing.Point(3, 249);
             this.label13.Name = "label13";
             this.label13.Size = new System.Drawing.Size(99, 21);
             this.label13.TabIndex = 9;
@@ -396,14 +372,13 @@
             this.ss_premiumIcon.Name = "ss_premiumIcon";
             this.ss_premiumIcon.Size = new System.Drawing.Size(320, 29);
             this.ss_premiumIcon.TabIndex = 8;
-            this.ss_premiumIcon.Text = "https://cdn.jsdelivr.net/gh/RED7Studios/RED7Community-CDN@main/assets/images/prem" +
-    "ium.png";
+            this.ss_premiumIcon.Text = "/assets/images/premium.png";
             // 
             // label9
             // 
             this.label9.AutoSize = true;
             this.label9.Font = new System.Drawing.Font("Segoe UI", 12F);
-            this.label9.Location = new System.Drawing.Point(6, 193);
+            this.label9.Location = new System.Drawing.Point(3, 193);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(110, 21);
             this.label9.TabIndex = 7;
@@ -422,7 +397,7 @@
             // 
             this.label10.AutoSize = true;
             this.label10.Font = new System.Drawing.Font("Segoe UI", 12F);
-            this.label10.Location = new System.Drawing.Point(6, 137);
+            this.label10.Location = new System.Drawing.Point(3, 137);
             this.label10.Name = "label10";
             this.label10.Size = new System.Drawing.Size(122, 21);
             this.label10.TabIndex = 5;
@@ -441,7 +416,7 @@
             // 
             this.label11.AutoSize = true;
             this.label11.Font = new System.Drawing.Font("Segoe UI", 12F);
-            this.label11.Location = new System.Drawing.Point(6, 81);
+            this.label11.Location = new System.Drawing.Point(3, 81);
             this.label11.Name = "label11";
             this.label11.Size = new System.Drawing.Size(97, 21);
             this.label11.TabIndex = 3;
@@ -460,7 +435,7 @@
             // 
             this.label12.AutoSize = true;
             this.label12.Font = new System.Drawing.Font("Segoe UI", 12F);
-            this.label12.Location = new System.Drawing.Point(6, 25);
+            this.label12.Location = new System.Drawing.Point(3, 27);
             this.label12.Name = "label12";
             this.label12.Size = new System.Drawing.Size(85, 21);
             this.label12.TabIndex = 1;
@@ -475,7 +450,7 @@
             this.gbMainUser.Font = new System.Drawing.Font("Segoe UI", 13F);
             this.gbMainUser.Location = new System.Drawing.Point(355, 280);
             this.gbMainUser.Name = "gbMainUser";
-            this.gbMainUser.Size = new System.Drawing.Size(336, 147);
+            this.gbMainUser.Size = new System.Drawing.Size(336, 157);
             this.gbMainUser.TabIndex = 17;
             this.gbMainUser.TabStop = false;
             this.gbMainUser.Text = "Main User:";
@@ -493,7 +468,7 @@
             // 
             this.label21.AutoSize = true;
             this.label21.Font = new System.Drawing.Font("Segoe UI", 12F);
-            this.label21.Location = new System.Drawing.Point(6, 81);
+            this.label21.Location = new System.Drawing.Point(3, 81);
             this.label21.Name = "label21";
             this.label21.Size = new System.Drawing.Size(76, 21);
             this.label21.TabIndex = 7;
@@ -512,7 +487,7 @@
             // 
             this.label24.AutoSize = true;
             this.label24.Font = new System.Drawing.Font("Segoe UI", 12F);
-            this.label24.Location = new System.Drawing.Point(6, 25);
+            this.label24.Location = new System.Drawing.Point(3, 25);
             this.label24.Name = "label24";
             this.label24.Size = new System.Drawing.Size(84, 21);
             this.label24.TabIndex = 1;
@@ -520,10 +495,11 @@
             // 
             // btnGenerate
             // 
-            this.btnGenerate.Font = new System.Drawing.Font("Segoe UI", 20F);
-            this.btnGenerate.Location = new System.Drawing.Point(355, 433);
+            this.btnGenerate.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGenerate.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.btnGenerate.Location = new System.Drawing.Point(697, 332);
             this.btnGenerate.Name = "btnGenerate";
-            this.btnGenerate.Size = new System.Drawing.Size(678, 65);
+            this.btnGenerate.Size = new System.Drawing.Size(336, 106);
             this.btnGenerate.TabIndex = 18;
             this.btnGenerate.Text = "GENERATE";
             this.btnGenerate.UseVisualStyleBackColor = true;
@@ -533,7 +509,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1044, 511);
+            this.ClientSize = new System.Drawing.Size(1044, 449);
             this.Controls.Add(this.btnGenerate);
             this.Controls.Add(this.gbMainUser);
             this.Controls.Add(this.gbSQLSettings);
@@ -576,8 +552,6 @@
         private System.Windows.Forms.TextBox ds_server;
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.GroupBox gbSQLSettings;
-        private System.Windows.Forms.TextBox ss_adminSiteName;
-        private System.Windows.Forms.Label label14;
         private System.Windows.Forms.TextBox ss_maintenanceMode;
         private System.Windows.Forms.Label label15;
         private System.Windows.Forms.TextBox ss_appealEmail;
