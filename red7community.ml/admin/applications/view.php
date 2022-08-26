e772cad373ec8fda74b5fd8cf56765c8bffe9362
Mitchell <68230846+Creaous@users.noreply.github.com>
2022-08-26 23:13:35 +0930

    Nearly left admin stuff open for everybody

--- red7community.ml/admin/applications/view.php
+++ red7community.ml/admin/applications/view.php
@@ -21,7 +21,7 @@ if (!str_contains($data, "This user doesn't exist or has been deleted")) {
     $role = $json[0]['data'][0]['role'];
 }
 
-if (false == true) { // (!$role >= 2) {
+if (!$role >= 2) {
     header("HTTP/1.1 403 Forbidden");
     exit;
 }
